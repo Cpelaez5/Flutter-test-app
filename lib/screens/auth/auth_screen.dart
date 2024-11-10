c1c51a21257b79b7f2c0545465d3bb33777c7a15
Cpelaez5 <cpelea121@gmail.com>
2024-11-09 20:51:08 -0400

    apply some fixes

--- lib/screens/auth/auth_screen.dart
+++ lib/screens/auth/auth_screen.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import '../../services/auth/auth_service.dart';
 import 'package:firebase_auth/firebase_auth.dart';
--- lib/utils/get_dolar_price.dart
+++ lib/utils/get_dolar_price.dart
@@ -1,6 +1,5 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<String> getDolarPrice() async {
   try {
